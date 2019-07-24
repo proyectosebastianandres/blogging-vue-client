ffa5fb26abdb8e11b8ab5a6d1f45f3698f821d4d
Sebastian Gutierrez <sebastiangutierrezcorrea@gmail.com>
2019-07-23 20:40:12 -0500

    Add markup for Login view

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Components from '@/views/Components'
+import Login from '@/views/Login'
 
 Vue.use(Router)
 
@@ -9,6 +10,11 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
     {
       path: '/components/',
       name: 'components',
---
+++ src/views/Login.vue
@@ -0,0 +1,88 @@
+<template>
+  <BCard class="login">
+    <div class="login-header">
+      <div class="logo">
+        <img src="logo.svg" alt="">
+      </div>
+      <div class="login__actions">
+        <a href="#" class="login__actions--current">Login</a>
+        <a href="/register">Singup</a>
+      </div>
+    </div>
+    <form name="login">
+      <BInput
+        placeholder="Email"
+        v-model="user.email"
+        :hint="messages.email"
+        type="email"
+      />
+      <BInput
+        placeholder="Password"
+        v-model="user.password"
+        :hint="messages.password"
+        type="password"
+      />
+      <BButton primary fullWidth type="submit">Login</BButton>
+    </form>
+  </BCard>
+</template>
+
+<script>
+import BInput from '@/components/BInput'
+import BButton from '@/components/BButton'
+import BCard from '@/components/BCard'
+
+export default {
+  name: 'Login',
+  components: {
+    BInput,
+    BButton,
+    BCard
+  },
+  data () {
+    return {
+      user: {
+        email: '',
+        password: ''
+      },
+      messages: {
+        email: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #fafafa;
+}
+
+.login {
+  max-width: 420px;
+  margin: auto;
+  margin-top: 120px;
+}
+
+.login-header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 2rem 0 1.5rem;
+}
+
+.login__actions a {
+  font-family: var(--sans-font);
+  text-decoration: none;
+  font-size: 1rem;
+  font-weight: bold;
+  color: var(--neutral-light);
+  padding: .5rem;
+}
+
+a.login__actions--current {
+  color: var(--primary);
+  pointer-events: none;
+}
+</style>
